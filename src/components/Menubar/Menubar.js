82f258f698de31456a41da81ee2928bf8cca525d
Ruman-Islam <rumanislam48@gmail.com>
2022-03-26 01:08:10 +0600

    gift data added

--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Menubar.css';
-import logo from '../../images/Logo.svg';
+import logo from '../../images/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping, faBars } from '@fortawesome/free-solid-svg-icons';
 
@@ -12,15 +12,15 @@ const Menubar = () => {
             </div>
             <input type="checkbox" name="" id="toggler" />
             <label className="nav-toggler-icon" for="toggler">
-                <FontAwesomeIcon icon={faBars} />
+                <FontAwesomeIcon id='bar-icon' icon={faBars} />
             </label>
             <div className='navigations'>
                 <a href="/shop">Shop</a>
                 <a href="/orders">Orders</a>
                 <a href="/inventory">Inventory</a>
                 <a href="/about">About</a>
-                <p className='cart-btn' href="#offcanvasExample" role="button">
-                    <FontAwesomeIcon className="icon" icon={faCartShopping} />
+                <p className='cart-btn' role="button">
+                    <FontAwesomeIcon icon={faCartShopping} />
                     <sup style={{ fontSize: '16px', marginLeft: '5px' }}>0</sup>
                 </p>
             </div>
